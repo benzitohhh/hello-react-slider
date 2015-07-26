29a9ea3ceba26468140f8a417d907aafe9974af5
Ben Immanuel <immanuel_ben@yahoo.co.uk>
2015-07-26 18:15:44 +0100

    rc-slider looks ok

--- src/index.js
+++ src/index.js
@@ -39,6 +39,4 @@ function onChange(v){
   console.log(v);
 }
 
-// React.render(<Slider marks={["一","二","三","四","五"]} index={3}/>, document.getElementById('__react-content'));
-// React.render(<Slider className='rc-slider' step={20}/>, document.getElementById('__react-content'));
 React.render(<div style={{width:400,margin:100}}><Slider onChange={onChange}/></div>, document.getElementById('container'));
